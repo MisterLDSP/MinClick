5e8847a80e11bb693ee33267a09baa056a2c4513
MisterLDSP <42472851+MisterLDSP@users.noreply.github.com>
2021-04-17 05:11:42 +0300

    Revert "Delete Product.cs"
    This reverts commit 67b9ffd3c408e8d83d6481a0bf886064e390d863.

---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinClick
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
