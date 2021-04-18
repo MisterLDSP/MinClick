f44711ed2007f3b596b2811a7f90fe6f3e070b5a
MisterLDSP <42472851+MisterLDSP@users.noreply.github.com>
2021-04-18 09:59:16 +0300

    Добавил категории
    Изменил класс работ, добавив лист категорий. Создал класс категорий с полями id и имя

---
+++ model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinClick.model
+{
+    /// <summary>
+    /// Класс для категорий работ
+    /// </summary>
+    class Category
+    {
+        public string categoryId { get; set; }
+        public string categoryName { get; set; }
+    }
+}
